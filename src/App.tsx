e329240eacca3f07be5aea023303d6a0976f7aea
rosnMagar <roshan.thamagar@gmail.com>
2026-03-05 11:30:52 -0600

    Auth Frontend

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,24 @@ import Outbound from './Pages/outbound'
 import ResidentLockout from './Pages/ResidentLockOut'
 import EquipmentCheckOut from './Pages/EquipmentCheckOut'
 import Timeclock from './Pages/Timeclock'
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ResidenceLife />} />
-        <Route path="/outbound" element={<Outbound />} />
-        <Route path="/lockout" element={<ResidentLockout />} />
-        <Route path="/equipment" element={<EquipmentCheckOut />} />
-        <Route path="/timeclock" element={<Timeclock />} />
-      </Routes>
-    </BrowserRouter>
+    <Authenticator>
+      {() => (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ResidenceLife />} />
+            <Route path="/outbound" element={<Outbound />} />
+            <Route path="/lockout" element={<ResidentLockout />} />
+            <Route path="/equipment" element={<EquipmentCheckOut />} />
+            <Route path="/timeclock" element={<Timeclock />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </Authenticator>
   )
 }
 
