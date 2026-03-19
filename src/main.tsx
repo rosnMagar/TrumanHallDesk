a3b7efd2b354a26cdf469e077bb733e02371d1c8
rosnMagar <roshan.thamagar@gmail.com>
2026-03-19 10:40:16 -0500

    Add Auth Features - login and signup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider, createTheme } from '@mantine/core'
 import '@mantine/core/styles.css'
+import { Amplify } from 'aws-amplify'
+import outputs from '../amplify_outputs.json'
 import App from './App'
+
+Amplify.configure(outputs)
 
 const theme = createTheme({
   primaryColor: 'grape',
