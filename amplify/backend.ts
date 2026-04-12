73e959fe214cae035aa483da80d899fcdb2e1b7a
rosnMagar <roshan.thamagar@gmail.com>
2026-04-11 20:18:21 -0500

    Lambda Integration SAM & CloudFormation

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,67 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { usersFunction } from './functions/usersApi/resource';
+import {
+  RestApi,
+  LambdaIntegration,
+  CognitoUserPoolsAuthorizer,
+  AuthorizationType,
+  Cors,
+} from 'aws-cdk-lib/aws-apigateway';
+import { CfnOutput } from 'aws-cdk-lib';
 
 /**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ * @see https://docs.amplify.aws/react/build-a-backend/
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  usersFunction,
 });
+
+const apiStack = backend.createStack('HallDeskApiStack');
+
+// 1. REST API
+const api = new RestApi(apiStack, 'HallDeskApi', {
+  restApiName: 'halldesk-api',
+  deployOptions: { stageName: 'prod' },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS,
+    allowMethods: Cors.ALL_METHODS,
+    allowHeaders: ['Authorization', 'Content-Type'],
+  },
+});
+
+// 2. Cognito authorizer
+const userPool = backend.auth.resources.userPool;
+const authorizer = new CognitoUserPoolsAuthorizer(apiStack, 'HallDeskAuthorizer', {
+  cognitoUserPools: [userPool],
+  authorizerName: 'HallDeskCognitoAuth',
+});
+
+const authOptions = {
+  authorizer,
+  authorizationType: AuthorizationType.COGNITO,
+};
+
+// 3. Mount Users Lambda
+const usersIntegration = new LambdaIntegration(backend.usersFunction.resources.lambda);
+const usersResource = api.root.addResource('users');
+
+// GET /users
+usersResource.addMethod('GET', usersIntegration, authOptions);
+
+// GET /users/{proxy+}
+const userProxy = usersResource.addProxy({
+  defaultIntegration: usersIntegration,
+  defaultMethodOptions: authOptions,
+  anyMethod: true,
+});
+
+// 4. Output API URL
+new CfnOutput(apiStack, 'HallDeskApiUrl', {
+  value: api.url,
+  description: 'API Gateway base URL',
+});
+
