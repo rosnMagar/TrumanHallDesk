8520c822313813f79d9e35575a63df3671eddebd
rosnMagar <roshan.thamagar@gmail.com>
2026-04-20 09:19:58 -0500

    clean backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -14,35 +14,3 @@ defineBackend({
   getPresignedUrl,
   deleteAvatar,
 });
-
-const apiStack = backend.createStack('HallDeskApiStack');
-
-// 1. REST API
-const api = new RestApi(apiStack, 'HallDeskApi', {
-  restApiName: 'halldesk-api',
-  deployOptions: { stageName: 'prod' },
-  defaultCorsPreflightOptions: {
-    allowOrigins: Cors.ALL_ORIGINS,
-    allowMethods: Cors.ALL_METHODS,
-    allowHeaders: ['Authorization', 'Content-Type'],
-  },
-});
-
-// 2. Cognito authorizer
-const userPool = backend.auth.resources.userPool;
-const authorizer = new CognitoUserPoolsAuthorizer(apiStack, 'HallDeskAuthorizer', {
-  cognitoUserPools: [userPool],
-  authorizerName: 'HallDeskCognitoAuth',
-});
-
-const authOptions = {
-  authorizer,
-  authorizationType: AuthorizationType.COGNITO,
-};
-
-// 4. Output API URL
-new CfnOutput(apiStack, 'HallDeskApiUrl', {
-  value: api.url,
-  description: 'API Gateway base URL',
-});
-
